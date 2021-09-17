d1c0a9222927349310d19e1fb885894abeb3d18d
Pratyaksh Gautam <pratyakshgautam11@gmail.com>
2021-09-17 23:37:18 +0530

    Add basic team member card structure

--- pages/team/index.js
+++ pages/team/index.js
@@ -1,5 +1,34 @@
+import Head from 'next/head';
+import styles from '../../styles/Team.module.css';
+
+const TeamMemberCard = (props) => {
+	return <>
+		<div className={`${styles.teamMember} px-5 py-5`}>
+			<div className={`${styles.teamMemberCardBackground} rounded h-75`}>
+				<div>
+					<img src='https://git-scm.com/images/logos/downloads/Git-Icon-1788C.png'
+						className='px-4'/>
+				</div>
+			</div>
+			<div className={`${styles.teamMemberNameTag} rounded border`}>
+				<h3 className='mx-2 my-auto'> {props.name} </h3>
+		    </div>
+		</div>
+	</>
+}
+
 const Team = () => {
-    return <h1>Team</h1>;
+	return <>
+		<Head>
+			<title>OSDG - Team</title>
+		</Head>
+		<div className={`${styles.topDiv} d-flex flex-column align-items-center`}>
+			<h1>Team</h1>
+		</div>
+		<div className='d-flex flex-column align-items-center'>
+			<TeamMemberCard name='Pratyay' />
+		</div>
+	</>
 };
 
 export default Team;
