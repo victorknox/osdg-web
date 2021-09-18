d264960e839b73400b8e3ff12b9ea6282aa46d27
Dhruv Kapur <dhruvkapur4@gmail.com>
2021-09-18 20:32:46 +0530

    Replaced MailGun with NodeMailer in the contact system

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,27 +1,28 @@
-const mailgun = require('mailgun-js');
-const client = mailgun({ apiKey: process.env.MAILGUN_API_KEY, domain: process.env.MAILGUN_DOMAIN });
+const nodemailer = require('nodemailer');
+
+const transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 587,
+    auth: {
+        user: process.env.CONTACT_EMAIL,
+        pass: process.env.CONTACT_PASSWORD
+    },
+});
 
 export default async function (req, res) {
 
-    const data = {
-        from: `${req.body.name} <${req.body.email}>`,
-        to: 'OSDG IIITH <osdg@students.iiit.ac.in>',
-        subject: 'Getting in touch',
-        text: `Hey OSDG Admins! You have a mail from ${req.body.name}. It reads: \n\n${req.body.query}`
+    const mailData = {
+        from: `"${req.body.name}" <${req.body.email}>`,
+        to: 'osdg@students.iiit.ac.in',
+        subject: 'Getting in Touch',
+        text: `Hey OSDG Admins! You have a mail from ${req.body.name}. It reads: \n\n${req.body.query}`,
+        replyTo: req.body.email
     };
-    try {
-        await new Promise((resolve, reject) => {
-            client.messages().send(data, function (error, body) {
-                if (error)
-                    reject(error);
-                else
-                    resolve(body);
 
-            });
-        });
+    try {
+        await transporter.sendMail(mailData);
         return res.status(200).json({ status: 'OK' });
-    }
-    catch (e) {
+    } catch (e) {
         return res.status(500).json({ error: e });
     }
 }
