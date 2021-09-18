e06acdc65f968addb6c82712ecfff210b1cfc03c
victorknox <vamshi.b@research.iiit.ac.in>
2021-09-18 13:03:46 +0530

    initial version

--- components/Project.jsx
+++ components/Project.jsx
@@ -1,3 +1,5 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFacebook, faGithub, faLinkedin, faInstagram } from "@fortawesome/free-brands-svg-icons";
 
 const Right = (props) => {
     const project = props.project;
@@ -13,13 +15,15 @@ const Right = (props) => {
                 <li className="badge bg-secondary">{project.tag3}</li>
               </ul>
               <p className="card-text">{project.body}</p>
-              {/* <a href="#" class="card-link">Website?</a> */}
-              <a href="#" className="card-link">Github link</a>
+            </div>
+            <div className="card-link d-flex justify-content-start" style={{marginLeft: '5px'}}>
+              <h2 className='social-link mx-2' onClick={() => { window.open('https://github.com/OSDG-IIITH', '_blank') }}><FontAwesomeIcon icon={faGithub} /></h2>
             </div>
           </div>
           <div className="col-md-6">
-            <img src="https://media.discordapp.net/attachments/779991636460699673/888509511252467772/unknown.png" className="img-fluid rounded-start" alt="..."/>
+            <img src="https://media.discordapp.net/attachments/779991636460699673/888509511252467772/unknown.png" className="img-fluid rounded project-image" alt="..."/>
           </div>
+          
         </div>
       </div>
     )
@@ -31,19 +35,21 @@ const Left = (props) => {
         <div className="card mb-3" style={{maxWidth: '60%' }} className="left-project">
         <div className="row g-0">
           <div className="col-md-6">
-            <img src="https://media.discordapp.net/attachments/779991636460699673/888509511252467772/unknown.png" className="img-fluid rounded-start" alt="..."/>
+            <img src="https://media.discordapp.net/attachments/779991636460699673/888509511252467772/unknown.png" className="img-fluid rounded project-image" alt="..."/>
           </div>
           <div className="col-md-6">
             <div className="card-body">
               <h5 className="card-title text-end">{project.title}</h5>
-              <ul className="tag-list">
+              <ul className="tag-list" >
                 <li className="badge bg-secondary">{project.tag1}</li>
                 <li className="badge bg-secondary">{project.tag2}</li>
                 <li className="badge bg-secondary">{project.tag3}</li>
               </ul>
               <p className="card-text">{project.body}</p>
               {/* <a href="#" class="card-link ">Website?</a> */}
-              <a href="#" className="card-link text-end">Github link</a>
+            <div className="card-link d-flex justify-content-end">
+              <h2 className='social-link mx-2' onClick={() => { window.open('https://github.com/OSDG-IIITH', '_blank') }}><FontAwesomeIcon icon={faGithub} /></h2>
+            </div>
             </div>            
           </div>
         </div>
--- pages/projects/index.js
+++ pages/projects/index.js
@@ -4,7 +4,7 @@ const Projects = () => {
     const projects = [
         {
             title: 'Project1', 
-            body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 
+            body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', 
             tag1: 'webdev', 
             tag2: 'MERN', 
             tag3: 'In progress', 
@@ -12,7 +12,7 @@ const Projects = () => {
         },
         {
             title: 'Project2', 
-            body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',  
+            body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', 
             tag1: 'webdev', 
             tag2: 'MERN', 
             tag3: 'In progress', 
@@ -20,7 +20,7 @@ const Projects = () => {
         },
         {
             title: 'Project3', 
-            body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',  
+            body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', 
             tag1: 'webdev', 
             tag2: 'MERN', 
             tag3: 'In progress', 
