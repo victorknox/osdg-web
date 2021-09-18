b4d64cf0ba802de734950bf3a2357869e6c991ad
Abhijnan Vegi <abhijnan.vegi@gmail.com>
2021-09-18 13:51:32 +0530

    Black buttons, hovering images, bold titles
    - Github buttons are now black
    - Project desc no longer overlap images
    - Project titles now in bold

--- components/Project.jsx
+++ components/Project.jsx
@@ -8,7 +8,7 @@ const Right = (props) => {
         <div className="row g-0">
           <div className="col-md-6">
             <div className="card-body">
-              <h5 className="card-title">{project.title}</h5>
+              <h5 className="card-title"><b>{project.title}</b></h5>
               <ul className="tag-list">
                 <li className="badge bg-secondary">{project.tag1}</li>
                 <li className="badge bg-secondary">{project.tag2}</li>
@@ -17,10 +17,10 @@ const Right = (props) => {
               <p className="card-text">{project.body}</p>
             </div>
             <div className="card-link d-flex justify-content-start" style={{marginLeft: '5px'}}>
-              <h2 className='social-link mx-2' onClick={() => { window.open('https://github.com/OSDG-IIITH', '_blank') }}><FontAwesomeIcon icon={faGithub} /></h2>
+              <h2 className='gh-link mx-2' onClick={() => { window.open('https://github.com/OSDG-IIITH', '_blank') }}><FontAwesomeIcon icon={faGithub} /></h2>
             </div>
           </div>
-          <div className="col-md-6">
+          <div className="col-md-6" style={{'z-index': '10'}}>
             <img src="https://media.discordapp.net/attachments/779991636460699673/888509511252467772/unknown.png" className="img-fluid rounded project-image" alt="..."/>
           </div>
           
@@ -34,12 +34,12 @@ const Left = (props) => {
     return (
         <div className="card mb-3" style={{maxWidth: '60%' }} className="left-project">
         <div className="row g-0">
-          <div className="col-md-6">
+          <div className="col-md-6" style={{'z-index': '10'}}>
             <img src="https://media.discordapp.net/attachments/779991636460699673/888509511252467772/unknown.png" className="img-fluid rounded project-image" alt="..."/>
           </div>
           <div className="col-md-6">
             <div className="card-body">
-              <h5 className="card-title text-end">{project.title}</h5>
+              <h5 className="card-title text-end"><b>{project.title}</b></h5>
               <ul className="tag-list" >
                 <li className="badge bg-secondary">{project.tag1}</li>
                 <li className="badge bg-secondary">{project.tag2}</li>
@@ -48,7 +48,7 @@ const Left = (props) => {
               <p className="card-text">{project.body}</p>
               {/* <a href="#" class="card-link ">Website?</a> */}
             <div className="card-link d-flex justify-content-end">
-              <h2 className='social-link mx-2' onClick={() => { window.open('https://github.com/OSDG-IIITH', '_blank') }}><FontAwesomeIcon icon={faGithub} /></h2>
+              <h2 className='gh-link mx-2' onClick={() => { window.open('https://github.com/OSDG-IIITH', '_blank') }}><FontAwesomeIcon icon={faGithub} /></h2>
             </div>
             </div>            
           </div>
